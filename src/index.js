4f94f4462f118cf963ab63e2fd217209f6bffd37
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2019-04-21 22:07:19 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 // @flow
 import React from 'react'
-import 'regenerator-runtime/runtime'
 import axios, { type $AxiosXHR } from 'axios'
+import 'regenerator-runtime/runtime'
+
+const HTTP_SUCCESS = 200
 
 export interface HttpClient {
   get<T>(url: string): Promise<$AxiosXHR<T>>;
 }
 
 export default function withHttp<P>(Component: React$ComponentType<P>) {
-  return class extends React.Component<P> implements HttpClient {
+  return class extends React.Component<P, {
+    isPending: boolean,
+    status: number,
+    statusText: string
+  }> implements HttpClient {
+    state = {
+      isPending: false,
+      status: HTTP_SUCCESS,
+      statusText: ''
+    }
+
+    // TODO: axios is not using the generic type 'T'
     request<T>(url: string, config?: {}): Promise<$AxiosXHR<T>> {
       return axios({
         url,
@@ -16,12 +29,31 @@ export default function withHttp<P>(Component: React$ComponentType<P>) {
       })
     }
 
-    get<T>(url: string, config?: {}): Promise<$AxiosXHR<T>> {
-      return this.request(url, config)
+    async get<T>(url: string, config?: {}): Promise<$AxiosXHR<T>> {
+      this.setState({ isPending: true, status: HTTP_SUCCESS, statusText: '' })
+      try {
+        return await this.request(url, config)
+      } catch (err) {
+        const { status, statusText } = err.response
+
+        this.setState({ status, statusText })
+        throw err
+      } finally {
+        this.setState({ isPending: false })
+      }
     }
 
     render() {
-      return <Component http={this} {...this.props} />
+      const { isPending, status, statusText } = this.state
+
+      return (
+        <Component
+          http={this}
+          isPending={isPending}
+          status={status}
+          statusText={statusText}
+          {...this.props} />
+      )
     }
   }
 }
